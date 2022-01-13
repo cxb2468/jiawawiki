9e902e2d9c1d10a824b8c3f1548d56d86146a9e7
Sakiel <cxb2468@qq.com>
2022-01-13 15:28:25 +0800

    2-7 使用HTTP 测试接口 test.http ;设置默认值@Value("${}")

--- src/main/java/com/jiawa/wiki/controlller/TestController.java
+++ src/main/java/com/jiawa/wiki/controlller/TestController.java
@@ -1,8 +1,8 @@
 package com.jiawa.wiki.controlller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,9 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value("${test.hello:TEST123}")
+    private String testHello;
+
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String Hello(){
-        return "Hello World";
+        return "Hello World " + testHello;
+    }
+
+    @RequestMapping(value = "/hello/post",method = RequestMethod.POST)
+    public String HelloPost(String name ){
+        return "Hello World  " + name;
     }
 
 }
